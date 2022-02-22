111b22524d1ef88a1c6dacdb815aadd46651662d
coryrobertson <cory.robertson17@gmail.com>
2022-02-22 13:30:53 -0800

    Initial commit with basic log system working

---
+++ src/main/java/com/github/coryrobertson/PainAnointer/LogLevels.java
@@ -0,0 +1,36 @@
+package com.github.coryrobertson.PainAnointer;
+
+import java.util.Date;
+
+public enum LogLevels
+{
+
+    ERROR, // used for actual scary errors, hopefully never have to use this
+    WARN, // used for small stuff like not knowing a specific state, but being able to recover
+    LOG; // used for logging basic messages
+
+    private static Date date = new Date();
+
+
+    public String getText()
+    {
+        String retn = "";
+        switch(this)
+        {
+            case ERROR:
+                retn += "[ERROR]\t";
+                break;
+            case WARN:
+                retn += "[WARN]\t";
+                break;
+            case LOG:
+                retn += "[LOG]\t\t";
+                break;
+            default:
+                retn += "[UNKNOWN LOG LEVEL]\t\t";
+                break;
+        }
+        retn = "[" + date.toString() + "]" + retn;
+        return retn;
+    }
+}
